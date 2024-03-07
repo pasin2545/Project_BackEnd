16d1f8d02aeb78a23d1bebbe958b82c12e84f847
0Thanapum2 <Thanapum@Satang.com>
2024-03-07 15:00:57 +0700

    none

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
 from routes.route import router
+from fastapi.middleware.cors import CORSMiddleware
 import os
 
 os.makedirs("data", exist_ok=True)
 
+
 app = FastAPI()
 
-app.include_router(router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods
+    allow_headers=["*"],  # Allow all headers
+)
 
+app.include_router(router)
